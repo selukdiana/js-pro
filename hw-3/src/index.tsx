8462056f5f52b7d6d7eb2c6c51210e6914e2f6dc
Diana Selyuk <selukdiana@gmail.com>
2022-06-06 12:37:43 +0300

    complete hw 36

--- hw-3/src/index.tsx
+++ hw-3/src/index.tsx
@@ -18,85 +18,178 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+// interface IUser {
+//   name: string;
+//   phone: string;
+//   email: string;
+//   animals?: string[];
+//   cars?: string[];
+//   hasChildren: boolean;
+//   hasEducation: boolean;
+// }
+
+// const users: Array<IUser> = [
+//   {
+//     name: 'Harry Felton',
+//     phone: '(09) 897 33 33',
+//     email: 'felton@gmail.com',
+//     animals: ['cat'],
+//     cars: ['bmw'],
+//     hasChildren: false,
+//     hasEducation: true,
+//   },
+//   {
+//     name: 'May Sender',
+//     phone: '(09) 117 33 33',
+//     email: 'sender22@gmail.com',
+//     hasChildren: true,
+//     hasEducation: true,
+//   },
+//   {
+//     name: 'Henry Ford',
+//     phone: '(09) 999 93 23',
+//     email: 'ford0@gmail.com',
+//     cars: ['bmw', 'audi'],
+//     hasChildren: true,
+//     hasEducation: false,
+//   },
+// ];
+
+// //     1. Создать строку из имен пользователей через запятую
+
+// function getUsernamesString(users: Array<IUser>): string {
+//   return users.map((user) => user.name).join(', ');
+// }
+
+// //     2. Посчитать общее количнство машин у пользователей
+
+// function getCarsTotalCount(users: Array<IUser>): number {
+//   return users.reduce((acc, user) => {
+//     return user.cars ? acc + user.cars.length : acc;
+//   }, 0);
+// }
+
+// //     3. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие образования
+
+// function filterUsersEducation(users: Array<IUser>): Array<IUser> {
+//   return users.filter((user) => user.hasEducation === true);
+// }
+
+// //     4. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие животных
+
+// function filterUsersAnimals(users: Array<IUser>): Array<IUser> {
+//   return users.filter((user) => user?.animals);
+// }
+
+// //     5. Создать функцию, которая бы принимала массив пользователей и отдавала бы  строку с названиями марок автомобилей через запятую
+
+// function getCarsString(users: Array<IUser>): string {
+//   const cars: Set<string> = new Set<string>();
+//   users.forEach((user) => {
+//     if (user.cars) {
+//       user.cars.forEach((car) => cars.add(car));
+//     }
+//   });
+//   return Array.from(cars).join(', ');
+// }
+
 interface IUser {
-  name: string;
-  phone: string;
+  id: number;
   email: string;
-  animals?: string[];
-  cars?: string[];
-  hasChildren: boolean;
-  hasEducation: boolean;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+  age: number;
 }
 
 const users: Array<IUser> = [
   {
-    name: 'Harry Felton',
-    phone: '(09) 897 33 33',
-    email: 'felton@gmail.com',
-    animals: ['cat'],
-    cars: ['bmw'],
-    hasChildren: false,
-    hasEducation: true,
+    id: 7,
+    email: 'michael.lawson@reqres.in',
+    first_name: 'Michael',
+    last_name: 'Lawson',
+    avatar: 'https://reqres.in/img/faces/7-image.jpg',
+    age: 23,
   },
   {
-    name: 'May Sender',
-    phone: '(09) 117 33 33',
-    email: 'sender22@gmail.com',
-    hasChildren: true,
-    hasEducation: true,
+    id: 8,
+    email: 'lindsay.ferguson@reqres.in',
+    first_name: 'Lindsay',
+    last_name: 'Ferguson',
+    avatar: 'https://reqres.in/img/faces/8-image.jpg',
+    age: 20,
   },
   {
-    name: 'Henry Ford',
-    phone: '(09) 999 93 23',
-    email: 'ford0@gmail.com',
-    cars: ['bmw', 'audi'],
-    hasChildren: true,
-    hasEducation: false,
+    id: 9,
+    email: 'tobias.funke@reqres.in',
+    first_name: 'Tobias',
+    last_name: 'Funke',
+    avatar: 'https://reqres.in/img/faces/9-image.jpg',
+    age: 40,
+  },
+  {
+    id: 10,
+    email: 'byron.fields@reqres.in',
+    first_name: 'Byron',
+    last_name: 'Fields',
+    avatar: 'https://reqres.in/img/faces/10-image.jpg',
+    age: 36,
+  },
+  {
+    id: 11,
+    email: 'george.edwards@reqres.in',
+    first_name: 'George',
+    last_name: 'Edwards',
+    avatar: 'https://reqres.in/img/faces/11-image.jpg',
+    age: 70,
+  },
+  {
+    id: 12,
+    email: 'rachel.howell@reqres.in',
+    first_name: 'Rachel',
+    last_name: 'Howell',
+    avatar: 'https://reqres.in/img/faces/12-image.jpg',
+    age: 45,
   },
 ];
 
-//     1. Создать строку из имен пользователей через запятую
+// 2. Создать массив из emails по алфавиту.
 
-function getUsernamesString(users: Array<IUser>): string {
-  return users.map((user) => user.name).join(', ');
+// const sortByEmail = (arr: Array<IUser>) => {
+//   arr
+//     .map((user: IUser) => user.email)
+//     .sort((a: string, b: string) => {
+//       a.localeCompare(b);
+//     });
+// };
+
+// 3. Создать новый массив пользователей, где объект пользователя должен содержать только id и поле, отвечающее за имя пользователя(например username), которое должно содержать имя и фамилию.
+interface INewUser {
+  id: number;
+  username: string;
 }
 
-//     2. Посчитать общее количнство машин у пользователей
-
-function getCarsTotalCount(users: Array<IUser>): number {
-  return users.reduce((acc, user) => {
-    return user.cars ? acc + user.cars.length : acc;
-  }, 0);
-}
-
-//     3. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие образования
-
-function filterUsersEducation(users: Array<IUser>): Array<IUser> {
-  return users.filter((user) => {
-    if (user.hasEducation) {
-      return user;
-    }
+const getNewArr = (arr: Array<IUser>): Array<INewUser> => {
+  return arr.map((user) => {
+    return { id: user.id, username: user.first_name + ' ' + user.last_name };
   });
-}
+};
 
-//     4. Создать функцию, которая бы принимала массив пользователей и отфильтровывала пользователей на наличие животных
+//превратить в ожну сртоку
 
-function filterUsersAnimals(users: Array<IUser>): Array<IUser> {
-  return users.filter((user) => {
-    if (user.animals) {
-      return user;
-    }
-  });
-}
+console.log(getNewArr(users));
 
-//     5. Создать функцию, которая бы принимала массив пользователей и отдавала бы  строку с названиями марок автомобилей через запятую
+// 4. Создать массив юзеров, где они отсортированы по возрасту по возрастанию и все пользователи младше 40 лет.
 
-function getCarsString(users: Array<IUser>): string {
-  const cars: Set<string> = new Set<string>();
-  users.forEach((user) => {
-    if (user.cars) {
-      user.cars.forEach((car) => cars.add(car));
-    }
-  });
-  return Array.from(cars).join(', ');
-}
+const sortByAge = (arr: Array<IUser>) => {
+  return arr.filter((user) => user.age < 40).sort((a, b) => a.age - b.age);
+};
+
+console.log(sortByAge(users));
+
+// 5. Получить объект, где были бы
+// a) данные о среднем возрасте пользователей b) количество пользователей старше 30
+// c) количество пользователей старше 40
+// d) количество пользователей старше 18
+// 6. Создать объект, где ключ, это первая буква фамилии, а значение - массив из фамилий пользователей начинающихся на эту букву. Объект должен состоять только из ключей существующих фамилий в этом массиве. Например в этом массиве нет фамилии с букву Y, а значит и такого поля не должно быть в объекте. Пример того, что надо получить, когда пользователи имеют следующие фамилии Snow, Felton , Ford, Ferdinand:
+// { s: [‘Snow’], f: ['Felton', 'Ford', 'Ferdinand' }
